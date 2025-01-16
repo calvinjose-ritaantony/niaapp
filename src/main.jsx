fd288ed8819a879d99af1f21f330dafd07973db8
Calvin <calvinjose.ritaantony@in.bosch.com>
2025-01-16 19:03:42 +0530

    MSAL Azure config Hard Coded

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,33 @@ import App from './App.jsx'
 import { MsalProvider } from "@azure/msal-react";
 import { PublicClientApplication } from "@azure/msal-browser";
 
-const client_id = `${import.meta.env.CLIENT_ID}`;
-const authority = `${import.meta.env.AUTHORITY_URL}`+"/"+`${import.meta.env.TENANT_ID}`;
-const redirect_uri = `${import.meta.env.REDIRECT_URI}`;
+// const client_id = `${import.meta.env.CLIENT_ID}`;
+// const authority = `${import.meta.env.AUTHORITY_URL}`+"/"+`${import.meta.env.TENANT_ID}`;
+// const redirect_uri = `${import.meta.env.REDIRECT_URI}`;
 
-const msalConfiguration = {
+const msalConfiguration_local = {
   auth: {
-      clientId : client_id,
-      authority: authority, // Replace with your Azure AD tenant ID
+      clientId : "53ddbbab-4e8b-4327-98d1-35c42d3329b3",
+      authority: "https://login.microsoftonline.com/6e50f04c-22d7-4b8e-8fa2-08f577dfa5aa", // Replace with your Azure AD tenant ID
       // the only mandatory field in this object, uniquely identifies your app
       // here you'll add the other fields that you might need based on the Azure portal settings
-      redirectUri: redirect_uri // Register as a SPA. This must match the redirect uri in Azure App Registration
+      redirectUri: "http://localhost:5173" // Register as a SPA. This must match the redirect uri in Azure App Registration
   }
 };
-const pca = new PublicClientApplication(msalConfiguration);
+
+const msalConfiguration_azure = {
+  auth: {
+      clientId : "5803bd0c-30b8-4f38-9327-ac6642e75245",
+      authority: "https://login.microsoftonline.com/6e50f04c-22d7-4b8e-8fa2-08f577dfa5aa", // Replace with your Azure AD tenant ID
+      // the only mandatory field in this object, uniquely identifies your app
+      // here you'll add the other fields that you might need based on the Azure portal settings
+      redirectUri: "https://niaapp-eaakgacxa4hsa3b4.australiaeast-01.azurewebsites.net" // Register as a SPA. This must match the redirect uri in Azure App Registration
+  }
+};
+
+//https://niaapp-eaakgacxa4hsa3b4.australiaeast-01.azurewebsites.net/.auth/login/aad/callback
+
+const pca = new PublicClientApplication(msalConfiguration_azure);
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
