12f4cd1f9dceffc1e04b1a7b870e1474ac407a3a
Shibaprassad Sahoo <external.Shibaprassad.Sahoo@bosch.com>
2025-03-05 10:20:08 +0530

    Menu structure updated for left panel
    Menu structure updated for left panel

--- src/components/LeftPanelComponent.jsx
+++ src/components/LeftPanelComponent.jsx
@@ -3,11 +3,47 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getUpdateInstructionAction, getUsecaseAction } from '../redux/actions/ChatConversationAction';
 import { formateChatHeadText } from '../utils/sharedFunction';
 
+const menuList = [
+  {
+    name : 'Marketing ',
+    useCases : ["GENERATE_MAIL_PROMOTION"]
+  },{
+    name: 'Commerce', 
+    useCases : [
+      "SUMMARIZE_PRODUCT_REVIEWS",
+      "PRODUCT_INFORMATION",
+      "PRODUCT_COMPARISON",
+      "CUSTOMIZED_RECOMMENDATIONS"
+    ]
+  },{
+    name : 'Orders',  
+    useCases : [
+      "SEARCHING_ORDERS",
+      "TRACK_ORDERS",
+      "GENERATE_MAIL_ORDERS"
+    ]
+  },{
+    name : 'ITSM',
+    useCases : [
+      "HANDLE_FAQS",
+      "CUSTOMER_COMPLAINTS",
+      "DOC_SEARCH",
+      "ANALYZE_SPENDING_PATTERNS",
+      "GENERATE_REPORTS"
+    ]
+ 
+  
+  }
+]
+
 const LeftPanelComponent = (props) => {
   const chatHead = useSelector((state)=>state.chatListData.chatHead);
   const userData = useSelector((state)=>state.userList.userInfo);
   const [selectedUseCase, setSelectedUseCase] = useState({});
-  const [chatHeadList, setChatHeadList] = useState(); //chatHead.length > 0 ? chatHead : chatHeadLists
+  const [chatHeadList, setChatHeadList] = useState("");
+  const [selectedChatHead, setSelectedChatHead] = useState("");
+  const [selectedUseCaseHead, setSelectedUseCaseHead] = useState("");
+  const [useCaseHeadList, setUseCaseHeadList] = useState("");
   const usecaseData = useSelector(state=>state.chatListData.chatUsecase);
   const dispatch = useDispatch();
   const getChatHead = async() =>{
@@ -19,8 +55,11 @@ const LeftPanelComponent = (props) => {
 
   useEffect(()=>{
     setChatHeadList(usecaseData);
-    setSelectedUseCase(usecaseData[0]);
-    props.selectListHandler(usecaseData[0]);
+    const selectedUseCaseData = usecaseData.find((item)=>item.name === menuList[0]?.useCases?.[0]);
+    setSelectedUseCase(selectedUseCaseData);
+    props.selectListHandler(selectedUseCaseData);
+    setSelectedChatHead(menuList[0].name);
+    setSelectedUseCaseHead(menuList[0]?.useCases?.[0]);
   },[usecaseData]);
 
   const selectUseCase = async(item) => {
@@ -29,18 +68,57 @@ const LeftPanelComponent = (props) => {
     props.selectListHandler(item);
   }
 
+
+  const checkMenuActive = (list) => {
+    if(selectedChatHead === list.name){
+      return true;
+    }else{
+      return false
+    }
+  }
+  const checkLevel2MenuActive = (item) => {
+    if(selectedUseCaseHead === item){
+      return true;
+    }else{
+      return false
+    }
+  }
+
+  const toggleMainMenu = (list) => {
+    setSelectedChatHead(list.name);
+    let item = 
+    toggleLevel2Menu(list, list.useCases?.[0]);
+    //setSelectedUseCaseHead(list.useCases?.[0]);
+  }
+  const toggleLevel2Menu = (list, item) => {
+    setSelectedChatHead(list.name);
+    setSelectedUseCaseHead(item);
+    const selectedChatHeadDetails = chatHeadList.find((items)=>items.name === item);
+    selectUseCase(selectedChatHeadDetails);
+  }
+
   return (
     <div className='nia-left-panel-container'>
         <div className='nia-left-panel-list'>
-            <ul>
-                {chatHeadList && chatHeadList.map((item,i)=> <Fragment  key={item._id}>
+            <ul className='nia-left-main-menu'>
+                {/* {chatHeadList && chatHeadList.map((item,i)=> <Fragment  key={item._id}>
                 <li className={`${item.name === selectedUseCase.name ? 'active' : ''}`} onClick={()=>selectUseCase(item)}>{formateChatHeadText(item.name)}
                     </li>
-                    {/* <ul>
-                      <li>Sub menus</li>
-                    </ul> */}
                     </Fragment>
-                 )}
+                 )} */}
+
+                 {menuList && menuList.map((list, i)=><Fragment key={`leftMainMenu_${i}`}>
+                  <li className={checkMenuActive(list) ? 'active' : ''} onClick={()=>toggleMainMenu(list)} >
+                    {formateChatHeadText(list.name)}
+                  </li>
+                  {list.useCases && list.useCases.length > 0 && <ul>
+                    {list.useCases.map((item,j)=><Fragment key={`level2_${j}`}>
+                      <li className={checkLevel2MenuActive(item) ? 'active' : ''} onClick={()=>{toggleLevel2Menu(list, item)}}>
+                      {formateChatHeadText(item)}
+                      </li>
+                    </Fragment>)}
+                  </ul>}
+                 </Fragment>)}
             </ul>
         </div>
     </div>
